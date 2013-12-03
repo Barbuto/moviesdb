6f2073ddd571abe9c815e00e5f337ec16fb6f794
Stephen Barbuto <you.barbutos92@gmail.com>
2013-12-02 22:03:47 -0500

    webpages

---
+++ home.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<div id= "header">
+	<h1>The Boots-Leg Movie Zone</h1>
+	</div>	
+	<link rel="stylesheet" type="text/css" href="movie.css"/>
+	</head>
+<div id= "container">
+
+<div id='cssmenu'>
+
+	<ul>
+		<li class="active"><a href="home.php"><span>Home</span></a></li>
+		<li><a href="movies.html"><span>Movies</span></a></li>
+		<li><a href="actors.html"><span>Actors & Actresses<span></a></li>
+		<li><a href="addmovie.html"><span>Add Movie<span></a></li>
+		<li><a href="addactors.html"><span>Add Actor or Actress<span></a></li>
+		<li class='last'> <a href="addstudio.html"><span>Add Studio<span></a></li>
+	</ul>
+</div>
+
+	<div id= "homebox">
+		<h1> This is the Boots-Leg Movie Website. </h1> </br>
+		<h3> A website to find any movie that comes to mind.</h3>
+	</div>
+	<div id="moviebox">
+		<p> Find A Movie!</p> 
+		<p>Search our site for a movie you would like</p>
+	
+	</div>
+	<div id="actorbox">
+		<p> Look for your Favorite Actor or Actress!</p>
+		<p> Find out more about them.</p> 
+	
+	</div>
+	<div id="addbox">
+	
+		<p> Add More!</p>
+	</div>
+	<div id="footer">
+	<p> By: Stephen Barbuto </p></br>
+	<p> A Student at the University of Scranton </p>
+	</div>
+	</div>
+	</body>
+</html>
